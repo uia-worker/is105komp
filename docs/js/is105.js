162489f3ab5f8f3d241b9d94a505fd8411f341fb
Janis Gailis <janis.gailis@uia.no>
2023-01-05 22:05:57 +0100

    Førse fullstendige

---
+++ docs/js/is105.js
@@ -0,0 +1,6 @@
+function TocviewToggle(glyph, id) {
+  var s = document.getElementById(id).style;
+  var expand = s.display == "none";
+  s.display = expand ? "block" : "none";
+  glyph.innerHTML = expand ? "&#9660;" : "&#9658;";
+}
